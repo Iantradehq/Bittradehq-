20c29ba811cb263729f708f94fc9b5f179cf500b
Iantradehq <ianmunene391@gmail.com>
2026-07-24 23:49:17 +0300

    Add login button click event with alert message

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const loginBtn = document.getElementById("loginBtn");
 
+loginBtn.addEventListener("click", () => {
+    alert("BitTradeHQ is connecting to Deriv...\n\nLogin integration is coming in the next step.");
+});
