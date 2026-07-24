0de5a19564d9495adaffc886c243bc599f945d62
Iantradehq <ianmunene391@gmail.com>
2026-07-24 21:45:17 +0300

    Add login button functionality to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const loginBtn = document.getElementById("loginBtn");
+const status = document.getElementById("status");
+
+loginBtn.addEventListener("click", () => {
+    status.innerText = "Deriv login will be added next.";
+});
