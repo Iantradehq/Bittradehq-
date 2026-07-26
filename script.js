73791472f6304baa2ab781d445466b015b6b8342
Iantradehq <ianmunene391@gmail.com>
2026-07-26 13:31:31 +0300

    Refactor login redirection to use WORKER_URL
    Updated login functionality to use WORKER_URL for redirection.

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-const APP_ID = "33VbNHVpYmxTsHQYWYy4j";
+const WORKER_URL = "https://bittradehq-auth.ianmunene391.workers.dev";
 
 const loginBtn = document.getElementById("loginBtn");
-const account = document.getElementById("account");
-const balance = document.getElementById("balance");
-const market = document.getElementById("market");
 
-loginBtn.addEventListener("click", () => {
-    const redirect = encodeURIComponent(window.location.href);
-
-    window.location.href =
-        `https://oauth.deriv.com/oauth2/authorize?app_id=${APP_ID}&l=EN&redirect_uri=${redirect}`;
-});
-
-const params = new URLSearchParams(window.location.search);
-
-if (params.get("token1")) {
-
-    account.innerHTML = "Connected to Deriv ✔";
-    balance.innerHTML = "Loading...";
-    market.innerHTML = "Connected";
-
-} else {
-
-    account.innerHTML = "Not connected";
-    balance.innerHTML = "--";
-    market.innerHTML = "Waiting for connection...";
-
+if (loginBtn) {
+    loginBtn.addEventListener("click", () => {
+        window.location.href = `${WORKER_URL}/login`;
+    });
 }
