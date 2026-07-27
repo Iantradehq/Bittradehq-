9e768735219e2ef55a5ab849dd3395eeafa8a422
Iantradehq <ianmunene391@gmail.com>
2026-07-27 22:24:20 +0300

    Refactor fetch function to handle multiple routes

--- worker/worker.js
+++ worker/worker.js
@@ -1,20 +1,133 @@
 export default {
-  async fetch(request) {
+  async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    return new Response(
-      JSON.stringify({
-        success: true,
-        project: "BitTradeHQ",
-        message: "Worker is running",
-        path: url.pathname
-      }),
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
-        }
+    try {
+      switch (url.pathname) {
+
+        case "/":
+          return json({
+            success: true,
+            project: "BitTradeHQ",
+            version: "1.0.0",
+            status: "Running"
+          });
+
+        case "/login":
+          return await login(request, env);
+
+        case "/callback":
+          return await callback(request, env);
+
+        case "/api/me":
+          return await me(request, env);
+
+        default:
+          return json({
+            success: false,
+            error: "Route not found"
+          }, 404);
+
       }
-    );
+
+    } catch (error) {
+
+      return json({
+        success: false,
+        message: error.message
+      }, 500);
+
+    }
+
   }
 };
+
+
+
+/* -------------------------------- */
+
+function json(data, status = 200) {
+
+  return new Response(
+
+    JSON.stringify(data, null, 2),
+
+    {
+      status,
+
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "*"
+      }
+
+    }
+
+  );
+
+}
+
+
+
+/* -------------------------------- */
+
+const CONFIG = {
+
+  CLIENT_ID: "33VbNHVpYmxTsHQYWYy4j",
+
+  REDIRECT_URI:
+    "https://bittradehq-auth-v2.ianmunene391.workers.dev/callback",
+
+  AUTH_URL:
+    "https://auth.deriv.com/oauth2/auth",
+
+  TOKEN_URL:
+    "https://auth.deriv.com/oauth2/token"
+
+};
+
+
+
+/* -------------------------------- */
+
+async function login(request, env) {
+
+  return json({
+
+    message:
+      "Login function will be completed in Part 2."
+
+  });
+
+}
+
+
+
+/* -------------------------------- */
+
+async function callback(request, env) {
+
+  return json({
+
+    message:
+      "Callback function will be completed in Part 3."
+
+  });
+
+}
+
+
+
+/* -------------------------------- */
+
+async function me(request, env) {
+
+  return json({
+
+    message:
+      "Account endpoint will be completed in Part 3."
+
+  });
+
+}
