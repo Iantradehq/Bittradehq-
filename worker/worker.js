ee3a518b51b87ec1ac005e21c10c4d82567028bc
Iantradehq <ianmunene391@gmail.com>
2026-07-27 21:21:30 +0300

    Initialize Bittradehq worker
    Added a fetch function to handle requests and respond with JSON.

--- worker/worker.js
+++ worker/worker.js
@@ -1,1 +1,20 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
 
+    return new Response(
+      JSON.stringify({
+        success: true,
+        project: "BitTradeHQ",
+        message: "Worker is running",
+        path: url.pathname
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      }
+    );
+  }
+};
